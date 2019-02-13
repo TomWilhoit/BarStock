e66914e104708e29d5935b8eff853c2d1ad1bb9f
Taylor Sperry <sperry.taylor@gmail.com>
2019-02-12 19:22:01 -0700

    Begin displayCategories() method

--- src/Inventory.js
+++ src/Inventory.js
@@ -8,26 +8,39 @@ class Inventory extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      allLiquor: [],
+      allBeer: []
     }
 
   }
 
   displayCategories() {
-    let testVar = this.props.allProducts[0].map( product => {
-      return product.category;
-    });
+    let typesArray = [];
+    this.props.allProducts[0].forEach((product, index) => {
+      if (!typesArray.includes(product.category)) {
+        typesArray.push(product.category);
+        typesArray.forEach((el, index) => {
+          return  <li key={index}>{product.type}</li>
+        })
+        // return <li key={index}>{product.type}</li>
+      }
+    })
+    console.log(typesArray);
   }
 
   render() {
     return (
       <div className="Inventory">
-        <h2>Inventory</h2>
-        { this.displayCategories() }
-        {/* { 
-          this.props.allProducts.map(product => {
-      return product.category})
-    } */}
+        <div className="liquor-toggles">
+          <section>Liquor</section>
+          <section>Beer</section>
+        </div>
+        <div className="display-liquor">
+        
+        </div>
+        <ul> 
+          { this.displayCategories() }
+        </ul>
       </div>
     );
   }
