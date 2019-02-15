4286561fc59e4011854daee312862c2ddfe890be
Michael Krog <michaelryankrog@gmail.com>
2019-02-15 16:27:26 -0700

    working branch updates to totals

--- src/Totals.js
+++ src/Totals.js
@@ -4,7 +4,6 @@ import App from './App.js';
 import Inventory from './Inventory';
 import './css/Totals.css';
 
-
 class Totals extends Component {
   constructor(props) {
     super(props);
@@ -14,55 +13,39 @@ class Totals extends Component {
     }
   }
 
-  
-
   totalCost = () => {
     return this.state.cartObjs.reduce((acc, currObj) => {
       return acc += currObj.price; 
     }, 0)
   }
 
-  
-
   totalProfit = () => {
-    let matchingObject; 
     return this.state.cartObjs.reduce((acc, currObj) => {
-      
-      
-  
-        matchingObject = this.state.cartNew.find(item => {
+        let menuObj = this.state.cartNew.find(item => {
           return item.inventory_code === currObj.inventory_code;
         });
-        
-        console.log(matchingObject.product)
-      
-      
-    
-        return acc; 
+        let dSize = currObj.size; // Distributor Product Serving Size
+        let mSize = menuObj.serving_size; // Menu Product Serving Size
+        let mPrice = menuObj.price_per_drink; // Menu Product Price Per Drink
+        let dCost = currObj.price; // Distributor Product Cost
+        return acc += (((dSize/mSize) * mPrice) - dCost); 
     }, 0)
   }
 
-
-
-  
   render() {
-    // console.log(this.state)
     return (
       <div className="Totals">
-        <h3>Total Cost</h3>
-        <h2>{this.totalCost()}</h2>
-        <br/>
-        <h3>Potential Profit</h3>
-        <h2>{this.totalProfit()}</h2>
-        {/* <h2>{this.totalProfit()}</h2> */}
+        <section>
+          <h3>Total Cost</h3>
+          <h2>${this.totalCost()}</h2>
+        </section>
+        <section>
+          <h3>Potential Profits</h3>
+          <h2>${(this.totalProfit().toFixed(2)) - this.totalCost()}</h2>
+        </section>
       </div>
     );
   }
 };
-
-
-
-
-
 
 export default Totals;
