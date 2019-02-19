c30fa08151e56cc8641a7150e8c7a0331eb1cba2
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-02-19 16:47:08 -0700

    App component testing

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -2,24 +2,23 @@ import React from 'react';
 import App from '../App';
 import { shallow } from 'enzyme';
 
-
-
 describe('App', () => {
   
   let wrapper;
+  const mockFunc = jest.fn();
+
   
   beforeEach(() => {
     wrapper = shallow(
-      <App/>
+      <App loginAccount = {
+        mockFunc}
+      />
       )
     })
   
-
-
   it('should match snapshot when all data is passed correctly', () => {
     expect(wrapper).toMatchSnapshot();
   });
-
 
   it('should have a proper default state', () => {
     expect(wrapper.state()).toEqual({
@@ -34,6 +33,22 @@ describe('App', () => {
       currentUser: ""
       })
     });
-  
 
+    it('should change the state of loginDisplay when invoked', () => {
+    expect(wrapper.state('loginDisplay')).toEqual(true);
+    wrapper.instance().toggleLogin();
+    expect(wrapper.state('loginDisplay')).toEqual(false);
+    })
+
+    it('should change the state of loginDisplay when invoked', () => {
+      expect(wrapper.state('currentUser')).toEqual('');
+      wrapper.instance().loginAccount('dang');
+      expect(wrapper.state('currentUser')).toEqual('dang');
+      })
+
+    // it('should alter the items displayed in your cart ', () => {
+    //   expect(wrapper.state('cartItems')).toEqual([]);
+    //   wrapper.instance().changeCart();
+    //   expect(wrapper.state('loginDisplay')).toEqual(false);
+    //   })
   });
